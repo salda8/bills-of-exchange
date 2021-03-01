5ddf301aa2aacc7f1f8ed90d9b4fc1d1c135b161
Maksym Prysiazhnyi <maksym.prysiazhnyi@valatron.com>
2021-03-01 23:05:56 +0100

    Fix data

--- Client/src/components/pages/Osoby.tsx
+++ Client/src/components/pages/Osoby.tsx
@@ -14,16 +14,13 @@ const useStyles = makeStyles({
     },
 });
 
-function createData(name: string, calories: number, fat: number, carbs: number, protein: number) {
-    return {name, calories, fat, carbs, protein};
+function createData(name: string, mail: string) {
+    return {name, mail};
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Jan Novotny', 'jan.novotny@gmail.com'),
+    createData('Petr Cech', 'petr.cech@gmail.com'),
 ];
 
 export default function Osoby() {
@@ -34,11 +31,8 @@ export default function Osoby() {
             <Table className={classes.table} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Jméno</TableCell>
+                        <TableCell align="right">Mail</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -47,10 +41,7 @@ export default function Osoby() {
                             <TableCell component="th" scope="row">
                                 {row.name}
                             </TableCell>
-                            <TableCell align="right">{row.calories}</TableCell>
-                            <TableCell align="right">{row.fat}</TableCell>
-                            <TableCell align="right">{row.carbs}</TableCell>
-                            <TableCell align="right">{row.protein}</TableCell>
+                            <TableCell align="right">{row.mail}</TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
